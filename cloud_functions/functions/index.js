264da6cdb5c85a659d2832dd75938b67f572086e
Kirin Patel <kirinpatel@gmail.com>
2017-11-11 17:07:36 -0500

    Changed functions

--- cloud_functions/functions/index.js
+++ cloud_functions/functions/index.js
@@ -1,44 +1,19 @@
 const functions = require('firebase-functions');
 const cors = require('cors')({origin: true});
 const admin = require('firebase-admin');
-const Firestore = require('@google-cloud/firestore');
+const firestore = require('@google-cloud/firestore');
 admin.initializeApp(functions.config().firebase);
 
 exports.analyzePost = functions.https.onRequest((req, res) => {
     cors(req, res, () => {
         const text = req.body;
-        res.status(200).send({data: 1});        
-    })
+        res.status(200).send("1");        
+    });
 });
 
-const firestore = new Firestore({
-  projectId: 'facebook_filter',
-  keyFilename: 'E:\Hackital2017\Facebook Filter-9777f0aec3cc.json',
-});
-
-const document = firestore.doc('posts/intro-to-firestore');
-
-// Enter new data into the document.
-document.set({
-  title: 'Welcome to Firestore',
-  body: 'Hello World',
-}).then(() => {
-  // Document created successfully.
-});
-
-// Update an existing document.
-document.update({
-  body: 'My first Firestore app',
-}).then(() => {
-  // Document updated successfully.
-});
-
-// Read the document.
-document.get().then(doc => {
-  // Document read successfully.
-});
-
-// Delete the document.
-document.delete().then(() => {
-  // Document deleted successfully.
+exports.testingFirestore = functions.https.onRequest((req, res) => {
+    cors(req, res, () => {
+        admin.firestore().collection('users').doc('Adam Rolek').update({testing: 'test'});
+        res.status(200).send();        
+    });
 });
